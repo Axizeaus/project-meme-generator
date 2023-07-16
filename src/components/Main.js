38b68ec8b5243f093b17700e3957b2c703baacf6
Axizeaus <axizeaus@gmail.com>
2023-07-16 14:11:05 +0630

    meme image working

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,24 @@
+import { useState } from "react";
+import memeData from "../memeData";
+
 export default function Main() {
-  console.log("main running");
+
+  const [memeImage, setMemeImage] = useState("http://i.imgflip.com/1bij.jpg");
+
+  function getRandomMeme(){
+    const memeArray = memeData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memeArray.length);
+    const url = memeArray[randomNumber].url
+    setMemeImage(url);
+  }
   return (
     <main>
-      <form className="form">
+      <div className="form">
         <input type="text" className="form--input1" placeholder="top text" />
         <input type="text" className="form--input2" placeholder="bottom text" />
-        <button className="form--button">make a meme</button>
-      </form>
+        <button className="form--button" onClick={getRandomMeme}>make a meme</button>
+      </div>
+      <img src={memeImage} alt="meme image" className="meme--image" />
     </main>
   );
 }
