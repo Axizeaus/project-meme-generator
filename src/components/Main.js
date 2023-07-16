d2db89fa2008c4d2d3cd0b6d45096413cf87cd23
Axizeaus <axizeaus@gmail.com>
2023-07-16 14:14:36 +0630

    refactor the state, now uses object

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,23 +2,28 @@ import { useState } from "react";
 import memeData from "../memeData";
 
 export default function Main() {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
 
-  const [memeImage, setMemeImage] = useState("http://i.imgflip.com/1bij.jpg");
-
-  function getRandomMeme(){
-    const memeArray = memeData.data.memes;
-    const randomNumber = Math.floor(Math.random() * memeArray.length);
-    const url = memeArray[randomNumber].url
-    setMemeImage(url);
+  function getMeme() {
+    const memesArray = memeData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMeme({ ...meme, randomImage: memesArray[randomNumber].url });
   }
+
   return (
     <main>
       <div className="form">
         <input type="text" className="form--input1" placeholder="top text" />
         <input type="text" className="form--input2" placeholder="bottom text" />
-        <button className="form--button" onClick={getRandomMeme}>make a meme</button>
+        <button className="form--button" onClick={getMeme}>
+          make a meme
+        </button>
       </div>
-      <img src={memeImage} alt="meme image" className="meme--image" />
+      <img src={meme.randomImage} alt="meme image" className="meme--image" />
     </main>
   );
 }
